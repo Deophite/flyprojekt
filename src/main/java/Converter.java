477c5917ef17a02a646bd1e6824c1a84db5ecf50
Alex <kensan32@netscape.net>
2025-08-26 14:17:18 +0200

    Converter gebaut. Funktioniert aus lokalem Ordner neu erstellt: dateixls & dateicsv converter kann Mit Variablen umgebaut werden um input zu definieren

---
+++ src/main/java/Converter.java
@@ -0,0 +1,34 @@
+import com.spire.xls.Workbook;
+import com.spire.xls.Worksheet;
+
+public class Converter {
+    public static void main(String[] args) {
+
+
+
+        //Variablen für die Input und Output Dateien. Bei Input muss noch geschaut werden, wie die Datei hinzugefügt wird für Variablen Namen.
+        String input = "src/main/dateixls/AA.xls";
+        String output = String.format("src/main/dateicsv/%s.csv", DateiName(input));
+
+        Workbook workbook = new Workbook();
+
+        //Die xls Datei wird hier geladen, der source woher er die Datei nimmt.
+        workbook.loadFromFile(input);
+
+        //Damit nimmt er die gesamte seite(sheet) der xls datei von "get(0)"
+        Worksheet sheet = workbook.getWorksheets().get(0);
+
+        //Hier speichert er die vorher ausgelesene Seite als csv Datei im Angegebenen Pfad und namen.
+        sheet.saveToFile(output,",");
+
+    }
+
+    //Funktion um den Dateinamen auszulesen.
+    public static String DateiName (String input) {
+
+        String dateiname = input.substring(input.lastIndexOf("/")+1, input.lastIndexOf("."));
+
+        return dateiname;
+    }
+
+}
