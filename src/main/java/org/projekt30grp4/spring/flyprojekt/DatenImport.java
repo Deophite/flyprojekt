c8bcb193492aa884d0517ba1db3ccd22a6085d32
Alex <kensan32@netscape.net>
2025-08-27 12:31:14 +0200

    DateiImport aufgebaut für die ersten 3 Tabellen

--- src/main/java/org/projekt30grp4/spring/flyprojekt/DatenImport.java
+++ src/main/java/org/projekt30grp4/spring/flyprojekt/DatenImport.java
@@ -1,6 +1,8 @@
 package org.projekt30grp4.spring.flyprojekt;
 
 import org.projekt30grp4.spring.flyprojekt.entities.Fluggesellschaft;
+import org.projekt30grp4.spring.flyprojekt.entities.Flughafen;
+import org.projekt30grp4.spring.flyprojekt.entities.Fluglinie;
 import org.projekt30grp4.spring.flyprojekt.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.math.BigDecimal;
+import java.time.LocalTime;
 
 @Service
 public class DatenImport {
@@ -48,7 +52,25 @@ public class DatenImport {
                 fg.setFluggesellschaftName(fluggesellschaftName);
                 fluggesellschaftRepository.save(fg);
 
+                // FLughafen
+                String flughafenName = data[3];
+                String stadt = data[5];
+                String land = data[4];
+                Flughafen fh =  flughafenRepository.findById(flughafenName).orElse(new Flughafen());
+                fh.setFlughafenName(flughafenName);
+                fh.setStadt(stadt);
+                fh.setLand(land);
+
                 //Fluglinie
+                int fl_id = Integer.parseInt(data[2]);
+                //String fh_von = data[3]; Das sind beide FK...
+                //String fh_nach = data[6];
+                LocalTime dauer = LocalTime.parse(data[9]);
+                BigDecimal preis = BigDecimal.valueOf(Double.parseDouble(data[11]));
+                Fluglinie fl = fluglinieRepository.findById(fl_id).orElse(new Fluglinie());
+                fl.setFl_ID(fl_id);
+                fl.setDauer(dauer);
+                fl.setPreis(preis);
 
             }
         }
--- src/main/java/org/projekt30grp4/spring/flyprojekt/repositories/FlughafenRepository.java
+++ src/main/java/org/projekt30grp4/spring/flyprojekt/repositories/FlughafenRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FlughafenRepository extends CrudRepository<Flughafen, Long> {
+public interface FlughafenRepository extends CrudRepository<Flughafen, String> {
 }
